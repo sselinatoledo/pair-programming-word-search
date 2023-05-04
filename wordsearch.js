25f865dbbb2d7f38ef11983166a4550fa4c61a77
sselinatoledo <sselinatoledo@gmail.com>
2023-05-04 06:56:03 +0000

    update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
-
-module.exports = wordSearch
+  }
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let col = "";
+    for (let j = 0; j < letters.length; j++) {
+      col += letters[j][i];
+    }
+    verticalJoin.push(col);
+  }
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+module.exports = wordSearch;
